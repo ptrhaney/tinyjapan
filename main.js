71cc9e98ae64d7c654f0da2ba7df0ba3363b028d
ptrhaney <84342049+ptrhaney@users.noreply.github.com>
2021-09-22 12:15:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -107,7 +107,7 @@ require([
 
   // define the earthquakes layer
   const earthquakeLayer = new CSVLayer({
-    url: "./earthquake_data_complete1973.csv",
+    url: "./tinyjapan.csv",
     elevationInfo: exaggeratedElevation,
     screenSizePerspectiveEnabled: false,
     renderer: {
@@ -133,8 +133,8 @@ require([
           field: "mag",
           axis: "all",
           stops: [
-            { value: 5.5, size: 70000, label: "<15%" },
-            { value: 7, size: 250000, label: "25%" }
+            { value: 3.5, size: 1000, label: "<15%" },
+            { value: 7, size: 5000, label: "25%" }
           ]
         },
         {
